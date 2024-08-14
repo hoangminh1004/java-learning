4fbd38654c38ee3d0ba8ef0906660dfc31a20816
MINH <hnfminhka@gmail.com>
2024-08-14 21:33:43 +0700

    minh - update sql

--- learning_mysql.sql
+++ learning_mysql.sql
@@ -1,1 +1,9 @@
-create database my_lerning;
+drop database if exists my_learning;
+create database if not exists my_learning;
+use my_learning;
+
+create table `department` ( 
+departmentId smallint unsigned auto_increment primary key,
+departmentName char(100)
+);
+ 
