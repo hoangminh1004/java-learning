65057abb27958c53d22570a2ed4fe5cd7d990acd
MINH <hnfminhka@gmail.com>
2024-08-15 13:58:07 +0700

    updae2

--- learning_mysql.sql
+++ learning_mysql.sql
@@ -1,1 +1,18 @@
-create database my_lerning;
+drop database if exists my_learning;
+create database if not exists my_learning;
+use my_learning;
+
+create table `department` ( 
+departmentId smallint unsigned auto_increment primary key,
+departmentName char(100)
+);
+ 
+create table `account` (
+accountId int unsigned auto_increment primary key , 
+accountEmail varchar(50) ,
+accountUsername  varchar(50) ,
+accountFullname varchar(50) ,
+departmentId int , 
+positionId int ,
+createDate tinyint
+);
